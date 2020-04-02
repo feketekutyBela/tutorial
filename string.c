7e54b856523210ed22df96dc8408d0993b9c75e2
feketekutyBela <feketekuty@yahoo.com>
2020-04-02 15:01:43 -0400

    Added my_strcpy function

--- string.c
+++ string.c
@@ -9,6 +9,19 @@ size_t my_strlen(char *s)
 
    return (p - s);
 }
+char *my_strcpy(char *t, char *s)
+
+{
+   
+   char *p = t;
+   
+   while (*t++ = *s++)
+   ;
+   
+   
+   return p;
+   
+}
 
 int main(void)
 {
@@ -22,6 +35,7 @@ int main(void)
    for (i = 0; i < 2; ++i)
       
    printf("string lenght of %s = %d\n", s[i], my_strlen(s[i]));
+   printf("%s\n", my_strcpy(p1, "Hello, World !!!"));
 
    return 0;
 }
